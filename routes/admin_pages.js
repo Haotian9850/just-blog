3058e640bdfe2423dcaad7ed615ecbd8d038fa77
Haotian9850 <haotian9850@gmail.com>
2018-11-03 11:43:46 -0400

    11-03-2018

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -29,8 +29,8 @@ router.get('/add-page', function(req, res){
         title: title,
         slug: slug,
         content: content
-    })
-})
+    });
+});
 
 /*
 * POST add page
@@ -112,6 +112,88 @@ router.post('/reorder-pages', function(req, res){
         })(count);
     }
 });
+
+
+/*
+ * GET edit page
+ */
+router.get('/edit-page/:slug', (req, res) => {
+    Page.findOne({slug : req.params.slug}).then((page) => {
+      if(!page) { //if page not exist in db
+        return res.status(404).send('Page not found');
+      }
+      res.render('admin/edit_page', { //page  exist
+        title: page.title,
+        slug: page.slug,
+        content: page.content,
+        id: page._id
+      });
+    }).catch((e) => {//bad request 
+      res.status(400).send(e);
+    });
+  });
+
+/*
+* POST edit page
+ */
+router.post('/edit-page/:slug', function(req, res){
+    //validate title and content
+    req.checkBody('title', 'Title must have a value').notEmpty();
+    req.checkBody('content', 'Content must have a value').notEmpty();
+
+    var title = req.body.title; //body parser
+    var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();   //replace all spaces with dash
+    if(slug == "") slug = title.replace(/\s+/g, '-').toLowerCase();
+
+    var content = req.body.content; 
+    var errors = req.validationErrors();
+    var id = req.body.id;
+
+    if(errors){
+        res.render('admin/add_page', {
+            errors: errors,
+            title: title,
+            slug: slug,
+            content: content
+        })
+    }else{
+        //make sure slug is unique
+        Page.findOne({slug: slug, _id: {'$ne':id}}, function(err, page){
+            if(page){
+                //we have a problem...
+                req.flash('danger', 'Page slug already exists. Choose another.');   //for BS msg styling
+                res.render('admin/add_page', {
+                    title: title,
+                    slug: slug,
+                    content: content,
+                    id: id
+                });
+            }else{
+                Page.findById(id, function(err, page){
+                    if(err) return console.log(err);
+                    page.title = title;
+                    page.slug = slug;
+                    page.content = content;
+                    //update page and save it to database
+                    page.save(function(err){
+                        if(err) return console.log(err);
+                        req.flash('success', 'Page added!');    //for BS msg styling
+                        res.redirect('/admin/edit-page/' + page.slug);
+                    });
+                });
+
+            }
+        });
+
+    }
+
+    res.render('admin/add_page', {
+        //print variable in the view
+        title: title,
+        slug: slug,
+        content: content
+    })
+})
 
 
 //exports
